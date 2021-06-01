572d48cde7cfa18c47bad7fc0dedb9e920f570fc
Joshua Fletcher <joshua.f1998@gmail.com>
2021-06-01 20:31:56 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function () {
 function loadImages() {
   const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
   fetch(imgUrl)
-    .then(res=> res.json())
+    .then(res => res.json())
     .then(results => {
       results.message.forEach(image => addImage(image))
     });
